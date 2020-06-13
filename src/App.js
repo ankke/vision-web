f0747d311674917dc1fd83314d2e8fadaa615b78
Anna Banaszak <a.banaszak@avsystem.com>
2020-06-13 14:49:31 +0200

    setup lint and prettier

--- src/App.js
+++ src/App.js
@@ -1,26 +1,21 @@
-import React from 'react';
-import './App.css';
-import { Provider } from 'react-redux';
+import gitReact from "react";
+import "./App.css";
+import { Provider } from "react-redux";
 import Example from "./screens/Example";
-import {
-    BrowserRouter as Router,
-    Switch,
-    Route
-} from "react-router-dom";
-import routes from './constants/routes'
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import routes from "./constants/routes";
 import Menu from "./components/Menu";
 
-
-function App({store, history}) {
+function App({ store, history }) {
   return (
-      <Provider store={store}>
-        <Router history={history}>
-            <Switch>
-                <Route exact path={routes.HOME} component={Example} />
-                <Route path={routes.EXAMPLE} component={Menu} />
-            </Switch>
-        </Router>
-      </Provider>
+    <Provider store={store}>
+      <Router history={history}>
+        <Switch>
+          <Route exact path={routes.HOME} component={Example} />
+          <Route path={routes.EXAMPLE} component={Menu} />
+        </Switch>
+      </Router>
+    </Provider>
   );
 }
 
